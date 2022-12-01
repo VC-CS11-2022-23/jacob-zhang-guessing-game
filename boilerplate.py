9391507f66b73d4c7f9fe20449b460baa0cfc458
project-cs11 <119448212+project-cs11@users.noreply.github.com>
2022-12-01 18:59:15 +0000

    done

--- boilerplate.py
+++ boilerplate.py
@@ -10,6 +10,22 @@ TRIES = 0
 
 # GAME LOOP
 while True:
+
+    if TRIES > 3:
+        print('you lose')
+        break
+
     guess = int(input("Guess a number between 1 and 10: "))
 
     # implement your logic below
+    if guess == number:
+        print("Correct")
+        break
+
+    if guess > number:
+        print("too high")
+
+    else:
+        print("too low")
+
+    TRIES = TRIES + 1
